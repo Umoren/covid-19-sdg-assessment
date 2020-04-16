6caf2e03c0a5981968cda14c36ec3a75abffeac3
Umoren <samuelumoren365@gmail.com>
2020-04-16 20:29:16 +0100

    Made some changes

--- src/estimator.js
+++ src/estimator.js
@@ -25,8 +25,8 @@ const covid19ImpactEstimator = (data) => {
   const severeHospitalBeds = Math.ceil(flooredBeds - severeSevereInfectionsTime);
   const impactIcuCases = (5 / 100) * impactInfectionsTime;
   const severeIcuCases = (5 / 100) * severeInfectionsTime;
-  const impactVentilatorCases = Math.trunc(Math.ceil((2 / 100) * impactInfectionsTime));
-  const severeVentilatorCases = Math.trunc(Math.ceil((2 / 100) * severeInfectionsTime));
+  const impactVentilatorCases = Math.trunc(Math.floor((2 / 100) * impactInfectionsTime));
+  const severeVentilatorCases = Math.trunc(Math.floor((2 / 100) * severeInfectionsTime));
   const avgUSD = data.region.avgDailyIncomeInUSD;
   const avgPOP = data.region.avgDailyIncomePopulation;
   const impactDollarsFlight = Math.trunc(impactInfectionsTime * avgUSD * avgPOP * checkPeriod());
